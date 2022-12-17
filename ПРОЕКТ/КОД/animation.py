8657cc1277926e79704f1ceb214105f244a4fc61
PitchBlackMilk <m.denisov16.01@gmail.com>
2022-12-17 11:05:22 +0300

    начало

---
+++ ПРОЕКТ/КОД/animation.py
@@ -0,0 +1,14 @@
+class Movement_Hor:
+    # todo горизонтальное движение с ускорением
+    pass
+
+
+# этот класс на тот случай если будет многоуровневая платформа
+class Falling:
+    # todo падение с скорением
+    pass
+
+
+class Bullet_Movement:
+    # todo работа с движением пуль
+    pass
