6e70f627a50b251f524a1620de9e5c990b654297
Roman <onlibrinty@gmail.com>
2022-12-18 20:09:20 +0300

    settings window was added

--- button.py
+++ button.py
@@ -4,20 +4,24 @@ pygame.init()
 
 class Button:
     def __init__(self, position, image, text):
+        self.text = text
         self.image = image
         self.rect = self.image.get_rect()   # Прямоугольник картинки
         self.rect.topleft = position    # Координата левого верхнего угла
 
         # Настройка шрифтов и размеров
-        font = int(self.rect.width // len(text) * 1.5)
+        font = int(self.rect.width // len(self.text) * 1.5)
         font = pygame.font.Font('assets/pixeboy.ttf', font)
-        self.text = font.render(text, True, 'white')
+        self.label = font.render(self.text, True, 'white')
 
-        text_width, text_height = font.size(text)   # Размеры текста
+        label_width, label_height = font.size(self.text)   # Размеры текста
         # Центрирование текста в кнопке
-        self.text_x = self.rect.x + (self.rect.width - text_width) // 2
-        self.text_y = self.rect.y + (self.rect.height - text_height) // 2
+        self.label_x = self.rect.x + (self.rect.width - label_width) // 2
+        self.label_y = self.rect.y + (self.rect.height - label_height) // 2
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-        screen.blit(self.text, (self.text_x, self.text_y))
+        screen.blit(self.label, (self.label_x, self.label_y))
+
+    def __call__(self):
+        return self.text
--- windows.py
+++ windows.py
@@ -1,6 +1,5 @@
 from button import Button
 import pygame
-pygame.init()
 
 WIDTH, HEIGHT = 1400, 1000
 EXTRA_WIDTH, EXTRA_HEIGHT = 600, 400
@@ -10,33 +9,81 @@ class ExtraWindow:
     screen = pygame.display.set_mode((EXTRA_WIDTH, EXTRA_HEIGHT))
     button_image = pygame.image.load('assets/button.png')
 
+    def __init__(self, buttons=(), labels=()):
+        pygame.init()
+
+        self.labels = labels
+        self.buttons = buttons
+
+        self.running = True
+        while self.running:
+            self.draw()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    self.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for button in self.buttons:
+                        if button.rect.collidepoint(event.pos):
+                            self.action(button())
+
+
+class SettingsWindow(ExtraWindow):
     def __init__(self):
         img_rect = self.button_image.get_rect()  # Размеры картинки
         center_x = (EXTRA_WIDTH - img_rect.width) // 2  # координата х для центрального расположения
 
-        self.start_button = Button((center_x, 50), self.button_image, 'Start Game')
-        self.continue_button = Button((center_x, 250), self.button_image, 'Continue Game')
+        easy_button = Button((center_x, 25), self.button_image, 'easy')
+        medium_button = Button((center_x, 150), self.button_image, 'medium')
+        hard_button = Button((center_x, 275), self.button_image, 'hard')
 
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    break
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.start_button.rect.collidepoint(event.pos):
-                        print('begin')
-                    elif self.continue_button.rect.collidepoint(event.pos):
-                        print('continue')
+        super().__init__((easy_button, medium_button, hard_button))
 
-            self.draw()
+    def action(self, button_text):
+        self.running = False
+        if button_text == 'easy':
+            pass
+        elif button_text == 'medium':
+            pass
+        elif button_text == 'hard':
+            pass
 
     def draw(self):
         self.screen.fill('white')
-        self.start_button.draw(self.screen)
-        self.continue_button.draw(self.screen)
+
+        for button in self.buttons:
+            button.draw(self.screen)
+
+        pygame.display.update()
+
+
+class StartWindow(ExtraWindow):
+    def __init__(self):
+        img_rect = self.button_image.get_rect()  # Размеры картинки
+        center_x = (EXTRA_WIDTH - img_rect.width) // 2  # координата х для центрального расположения
+
+        start_button = Button((center_x, 50), self.button_image, 'start game')
+        continue_button = Button((center_x, 250), self.button_image, 'continue game')
+
+        super().__init__((start_button, continue_button))
+
+    def action(self, button_text):
+        self.running = False
+
+        if button_text == 'start game':
+            SettingsWindow()
+        elif button_text == 'continue game':
+            pass
+
+    def draw(self):
+        self.screen.fill('white')
+
+        for button in self.buttons:
+            button.draw(self.screen)
 
         pygame.display.update()
 
 
 if __name__ == '__main__':
-    ExtraWindow()
+    StartWindow()
