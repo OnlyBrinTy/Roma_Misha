2a19ced4481fa87201b75b4b36070f5bfa652e9f
PitchBlackMilk <m.denisov16.01@gmail.com>
2022-12-19 20:52:29 +0300

    added few classes, buttons positions where changed, added temporary background color

--- windows.py
+++ windows.py
@@ -3,12 +3,12 @@ import pygame
 
 WIDTH, HEIGHT = 1400, 1000
 EXTRA_WIDTH, EXTRA_HEIGHT = 600, 400
-background_color = (255, 255, 255)
+background_color = (41, 52, 80)
 
 
 class ExtraWindow:
-    screen = pygame.display.set_mode((EXTRA_WIDTH, EXTRA_HEIGHT))
-    button_image = pygame.image.load('assets/button.png')
+    screen = pygame.display.set_mode((EXTRA_WIDTH, EXTRA_HEIGHT))  # Создание экрана с заданными рамерами
+    button_image = pygame.image.load('assets/button.png')  # Загрузка заднего фона кнопки
 
     def __init__(self, buttons=(), labels=()):
         pygame.init()
@@ -17,7 +17,7 @@ class ExtraWindow:
         self.buttons = buttons
 
         self.running = True
-        while self.running:
+        while self.running:  # Игровой цикл
             self.draw()
 
             for event in pygame.event.get():
@@ -35,13 +35,13 @@ class SettingsWindow(ExtraWindow):
         img_rect = self.button_image.get_rect()  # Размеры картинки
         center_x = (EXTRA_WIDTH - img_rect.width) // 2  # координата х для центрального расположения
 
-        easy_button = Button((center_x, 25), self.button_image, 'easy')
-        medium_button = Button((center_x, 150), self.button_image, 'medium')
-        hard_button = Button((center_x, 275), self.button_image, 'hard')
+        easy_button = Button((center_x, 30), self.button_image, 'easy')  # Кнопка для легкой сложности
+        medium_button = Button((center_x, 154), self.button_image, 'medium')  # Кнопка для средней сложности
+        hard_button = Button((center_x, 278), self.button_image, 'hard')  # Кнопка для хардкора
 
         super().__init__((easy_button, medium_button, hard_button))
 
-    def action(self, button_text):
+    def action(self, button_text):  # Обработка нажатий на кнопки
         self.running = False
         if button_text == 'easy':
             pass
@@ -64,17 +64,17 @@ class StartWindow(ExtraWindow):
         img_rect = self.button_image.get_rect()  # Размеры картинки
         center_x = (EXTRA_WIDTH - img_rect.width) // 2  # координата х для центрального расположения
 
-        start_button = Button((center_x, 50), self.button_image, 'start game')
-        continue_button = Button((center_x, 250), self.button_image, 'continue game')
+        start_button = Button((center_x, 69), self.button_image, 'start')
+        continue_button = Button((center_x, 232), self.button_image, 'continue')
 
         super().__init__((start_button, continue_button))
 
     def action(self, button_text):
         self.running = False
 
-        if button_text == 'start game':
+        if button_text == 'start':  # Обработка нажатия на кнопку start
             SettingsWindow()
-        elif button_text == 'continue game':
+        elif button_text == 'continue':  # Обработка нажатия на кнопку continue
             pass
 
     def draw(self):
@@ -84,6 +84,14 @@ class StartWindow(ExtraWindow):
             button.draw(self.screen)
 
         pygame.display.update()
+
+
+class MenuWindow(ExtraWindow):
+    pass
+
+
+class UpgradeWindow(ExtraWindow):
+    pass
 
 
 if __name__ == '__main__':
