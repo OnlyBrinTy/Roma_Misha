ff10730490a5b34ba3ee77d3d843bc5c1a71c168
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-01-05 13:44:14 +0300

    weapon works strangely

--- weapon.py
+++ weapon.py
@@ -1,51 +1,36 @@
 import pygame
+from entities import *
+from texture import Texture
+from rectangle import *
 
 pygame.init()
-text_color = (3, 112, 207)
-background_color = (255, 255, 255)
+TEXT_COLOR = (3, 112, 207)
 
 
 class Weapon:
-    def __init__(self, screen):
-        self.screen = screen
-        bullet = BulletAmount()
-        bullet_amo = bullet.get_bullet()
-        self.font = pygame.font.Font('assets/pixeboy.ttf', 35)
-
-        amo_label = self.font.render(str(bullet_amo), True, text_color)
-        self.screen.blit(amo_label, (10, 10))
-
-        pygame.display.update()
-
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        if bullet_amo > 0:
-                            self.draw(bullet_amo)
-
-    def draw(self, bullet_amo):
-        amo_label = self.font.render(str(bullet_amo), True, text_color)
-        self.screen.blit(amo_label, (10, 10))
+    pass
 
 
 class CurrentWeapon:
     pass
 
 
-class Bullet(pygame.sprite.Sprite):
-    pass
+class Bullet(Entity):
+    def __init__(self, start_pos, finish_pos, file_name, group, screen_size):
+        self.MAX_SPEED = 50
+        # bullets = pygame.sprite.Group()
+        self.image = pygame.image.load(file_name)
+        self.rect = self.image.get_rect(topleft=start_pos)
+        self.final_pos = finish_pos[0] - screen_size[0] // 2, finish_pos[1] - screen_size[1] // 2
+        self.velocity_x, self.velocity_y = self.final_pos[0] // 10, self.final_pos[1] // 10
+        Entity.__init__(self, start_pos, file_name, group)
+
+    def motion(self, slowdown):
+        self.add_rect = Rect(self.rect)
+
+    def get_final_pos(self):
+        return self.final_pos
 
 
 class BulletAmount:
-    def __init__(self):
-        self.bullet = 10
-        self.font = pygame.font.Font('assets/pixeboy.ttf', 35)
-
-    def get_bullet(self):
-        self.bullet -= 1
-        amo_label = self.font.render(str(self.bullet), True, text_color)
-
-        return amo_label
+    pass
